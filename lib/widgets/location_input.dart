07f2b6885ac196dcf8f3f18932e5d072cd6c4ebb
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-23 06:43:46 +0300

    getting users current location

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -8,8 +9,72 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+  Location? pickedLocation; // Variable to store the user's picked location data
+  var _isGettingLocation =
+      false; // Flag to indicate if the location is being fetched
+
+// Method to get the user's current location
+  void _getCurrentLocation() async {
+    Location location =
+        Location(); // Create a Location instance to access location services
+
+    bool
+        serviceEnabled; // Variable to store whether location services are enabled
+    PermissionStatus
+        permissionGranted; // Variable to store location permission status
+    LocationData locationData; // Variable to store the retrieved location data
+
+    // Check if location services are enabled
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      // Request to enable location services if they are not enabled
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        // If the user refuses to enable location services, exit the method
+        return;
+      }
+    }
+
+    // Check if the app has location permissions
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      // Request location permissions if they are denied
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        // If the user refuses to grant location permissions, exit the method
+        return;
+      }
+    }
+
+    // Update the state to indicate that the app is in the process of getting the location
+    setState(() {
+      _isGettingLocation = true; // Start the loading indicator
+    });
+
+    // Retrieve the current location of the device
+    locationData = await location.getLocation();
+
+    // Update the state with the retrieved location data
+    setState(() {
+      _isGettingLocation = false; // Stop the loading indicator
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      'No location choosen',
+      textAlign: TextAlign.center,
+      style: Theme.of(context)
+          .textTheme
+          .bodyLarge!
+          .copyWith(color: Theme.of(context).colorScheme.tertiary),
+    );
+
+    if (_isGettingLocation) {
+      previewContent = const CircularProgressIndicator.adaptive();
+    }
+
     return Column(
       children: [
         Container(
@@ -25,20 +90,13 @@ class _LocationInputState extends State<LocationInput> {
                   .withOpacity(0.2), // Add a border with slight opacity
             ),
           ),
-          child: Text(
-            'No location choosen',
-            textAlign: TextAlign.center,
-            style: Theme.of(context)
-                .textTheme
-                .bodyLarge!
-                .copyWith(color: Theme.of(context).colorScheme.tertiary),
-          ),
+          child: previewContent,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
               icon: Icon(Icons.location_on_outlined),
               label: Text('Get location'),
             ),
